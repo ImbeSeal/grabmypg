04c62136913925d7b98341008446f41df941d37b
ImbeSeal <saket.zingade@gmail.com>
2024-03-18 09:16:51 +0530

    Added Landlord's display page

--- src/app/pages/Card.tsx
+++ src/app/pages/Card.tsx
@@ -2,8 +2,10 @@ import Image from "next/image";
 
 export default function Card({ image, head, body }) {
   return (
-    <div className="p-8 space-y-4 bg-white rounded-3xl shadow-lg text-left">
-      <Image src={image} alt="..." className="w-[80px] aspect-square" />
+    <div className="p-8 space-y-4">
+      <div className="w-full">
+        <Image src={image} alt="..." className="w-[80px] aspect-square" />
+      </div>
       <div className="text-xl font-bold">{head}</div>
       <div className="text-normal">{body}</div>
     </div>
