25d799944774c36e9a14007423a65dcbe80e9e39
ImbeSeal <saket.zingade@gmail.com>
2024-03-19 14:08:58 +0530

    Added basic data to be added to the prop card

--- src/app/pages/Booking.tsx
+++ src/app/pages/Booking.tsx
@@ -5,13 +5,31 @@ export function Booking() {
     <div className="bg-[#F0F0F0]">
       <div className="w-2/3 ml-auto mr-12 py-3 grid grid-cols-1 gap-4">
         <a href="#">
-          <PropCard uid="1" />
+          <PropCard
+            uid="1"
+            propName={"Ok"}
+            address={"This place"}
+            price={3000}
+            rating={4.0}
+          />
         </a>
         <a href="#">
-          <PropCard uid="2" />
+          <PropCard
+            uid="2"
+            propName={"Ok"}
+            address={"This place"}
+            price={3000}
+            rating={3.5}
+          />
         </a>
         <a href="#">
-          <PropCard uid="3" />
+          <PropCard
+            uid="3"
+            propName={"Ok"}
+            address={"This place"}
+            price={3000}
+            rating={2.9}
+          />
         </a>
       </div>
     </div>
--- src/app/pages/CardProperty.tsx
+++ src/app/pages/CardProperty.tsx
@@ -6,24 +6,62 @@ import food from "../media/icons/propcardVeg.png";
 import gender from "../media/icons/propcardBoysGirls.png";
 import occupation from "../media/icons/propcardStudents.png";
 
-export function PropCard({ uid }) {
+export function PropCard({ uid, propName, address, price, rating }) {
+  // function to assing the stars
+  function Star({ value }: { value: number }) {
+    if (value < 1)
+      return (
+        <svg
+          className="w-4 h-4 ms-1 text-gray-300 dark:text-gray-500"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="currentColor"
+          viewBox="0 0 22 20"
+        >
+          <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+        </svg>
+      );
+    else
+      return (
+        <svg
+          className="w-4 h-4 text-[#2B67F6] ms-1"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="currentColor"
+          viewBox="0 0 22 20"
+        >
+          <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+        </svg>
+      );
+  }
+
   return (
     <div className="grid grid-cols-4 p-12 bg-white rounded-[24px] shadow-lg">
       <div className="col-span-1">
-        <Image src={prop1} alt=".." className="aspect-square rounded-[12px]" />
+        <div className="relative">
+          <Image
+            src={prop1}
+            alt=".."
+            className="aspect-square rounded-[12px]"
+          />
+          <div className="absolute ml-2 bottom-4">
+            <div className="font-semibold text-white text-sm">2 Roommates</div>
+            <div className="text-white text-xs opacity-65">Names of People</div>
+          </div>
+        </div>
       </div>
 
       {/* Details */}
 
       <div className="col-span-2 ml-6">
         {/* Name  */}
-        <div className="text-xl font-semibold">Room in boutique building</div>
+        <div className="text-xl font-semibold">{propName}</div>
         {/* Location  */}
         <a href={"#"} className="text-sm mt-2">
-          1995 Broadway, New York
+          {address}
         </a>
         {/* Features  */}
-        <div className="grid grid-rows-3 grid-flow-col gap-y-2 mt-2">
+        <div className="grid grid-rows-3 grid-flow-col gap-y-1 mt-2">
           <div className="flex gap-2 mx-3 ">
             <Image
               src={food}
@@ -74,7 +112,7 @@ export function PropCard({ uid }) {
 
         {/* Contact  */}
 
-        <div className="grid grid-cols-2 mx-4 gap-4 mt-4">
+        <div className="grid grid-cols-2 mx-4 gap-4 mt-2">
           <button className="p-1 rounded-[10px] bg-[#2B67F6] font-semibold">
             Contact Owner
           </button>
@@ -84,55 +122,17 @@ export function PropCard({ uid }) {
         </div>
 
         {/* Rating */}
-        <div className="flex gap-4 mt-2">
-          <div className="">5.0</div>
+        <div className="flex gap-4 mt-3">
+          <div className="">{rating}</div>
+
+          {/* Stars can be changed on the basis of value */}
 
           <div className="flex items-center">
-            <svg
-              className="w-4 h-4 text-[#2B67F6] ms-1"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 22 20"
-            >
-              <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-            </svg>
-            <svg
-              className="w-4 h-4 text-[#2B67F6] ms-1"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 22 20"
-            >
-              <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-            </svg>
-            <svg
-              className="w-4 h-4 text-[#2B67F6] ms-1"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 22 20"
-            >
-              <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-            </svg>
-            <svg
-              className="w-4 h-4 text-[#2B67F6] ms-1"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 22 20"
-            >
-              <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-            </svg>
-            <svg
-              className="w-4 h-4 ms-1 text-gray-300 dark:text-gray-500"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 22 20"
-            >
-              <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-            </svg>
+            <Star value={rating / 1} />
+            <Star value={rating / 2} />
+            <Star value={rating / 3} />
+            <Star value={rating / 4} />
+            <Star value={rating / 5} />
           </div>
 
           <div className="text-[0.7rem] my-auto">(42 Reviews)</div>
@@ -159,7 +159,8 @@ export function PropCard({ uid }) {
         </div>
         <div className="flex flex-col-reverse h-1/2">
           <div className="font-bold mt-auto text-end ">
-            Rs {`3,000`} <span className="text-xs font-normal">/month</span>
+            {/* Input for cost */}
+            Rs {price} <span className="text-xs font-normal">/month</span>
           </div>
         </div>
       </div>
