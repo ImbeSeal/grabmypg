b3d4990cf6f0ec3560f9b2d74a621ae273af7f27
ImbeSeal <saket.zingade@gmail.com>
2024-04-23 22:46:51 +0530

    Added modal to Contact in Property details

--- src/app/PropDetails/page.tsx
+++ src/app/PropDetails/page.tsx
@@ -18,6 +18,8 @@ import Carousel from "@/components/Carousel";
 import hostel2 from "../assets/hostel-bg.jpg";
 
 export default function Details() {
+  const [showModal, setShowModal] = useState(false);
+
   const [date, setDate] = useState(new Date());
   const today = new Date();
 
@@ -57,7 +59,6 @@ export default function Details() {
   if (!details) {
     return <p>Details not found.</p>;
   }
-
 
   return (
     <main className="bg-white bg-opacity-55 text-black flex justify-center">
@@ -124,15 +125,55 @@ export default function Details() {
                     Schedule meeting
                   </div>
                 </div>
-                <div className="text-center px-16 text-xs">
+                <button
+                  className="text-center px-16 text-xs"
+                  type="button"
+                  onClick={() => setShowModal(true)}
+                >
                   <div className="bg-black text-white rounded-full py-2 px-8">
                     Contact owner
                   </div>
-                </div>
+                </button>
+                {showModal ? (
+                  <>
+                    <div className="flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none bg-white bg-opacity-70">
+                      <div className="relative w-auto my-6 mx-auto max-w-3xl">
+                        <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                          <div className="flex items-start justify-between p-5 border-b border-solid border-gray-300 rounded-t ">
+                            <h3 className="text-3xl font=semibold">
+                              {details.title}
+                            </h3>
+                            <button
+                              className="bg-transparent border-0 text-black float-right"
+                              onClick={() => setShowModal(false)}
+                            >
+                              <span className="grid text-black opacity-7 h-8 w-8 text-xl bg-gray-400 py-0 rounded-full justify-center my-auto mx-auto">
+                                <svg
+                                  xmlns="http://www.w3.org/2000/svg"
+                                  viewBox="0 0 384 512"
+                                  className="h-4 w-4 mt-2"
+                                >
+                                  <path d="M376.6 84.5c11.3-13.6 9.5-33.8-4.1-45.1s-33.8-9.5-45.1 4.1L192 206 56.6 43.5C45.3 29.9 25.1 28.1 11.5 39.4S-3.9 70.9 7.4 84.5L150.3 256 7.4 427.5c-11.3 13.6-9.5 33.8 4.1 45.1s33.8 9.5 45.1-4.1L192 306 327.4 468.5c11.3 13.6 31.5 15.4 45.1 4.1s15.4-31.5 4.1-45.1L233.7 256 376.6 84.5z" />
+                                </svg>
+                              </span>
+                            </button>
+                          </div>
+                          <div className="relative p-6 flex-auto">
+                            {/* Requires addition of contact Name Number and Email */}
+                            <div className="">Name of Owner: contact.name</div>
+                            <div className="">Whatsapp: contact.number</div>
+                            <div className="">Email: contact.email</div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </>
+                ) : null}
               </div>
 
               <div className="px-16 text-xs text-center my-4">
-                When you schedule this meeting, the PG owner will reach out to you.
+                When you schedule this meeting, the PG owner will reach out to
+                you.
               </div>
             </div>
           </div>
